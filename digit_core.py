d1246a28c97f2599e07bcf5d404adc99f23da893
Loki Davison <loki.davison@gravityfour.com>
2019-01-06 22:21:22 +1100

    added core outline

---
+++ digit_core.py
@@ -0,0 +1,41 @@
+# 26 total effect slots? 
+# delay 1-4
+# reverb
+# cab
+# effects 1-4 per delay & reverb
+
+## on ui knob change, set value in lv2
+def ui_knob_change(value):
+    # knob is zero to 1, map to pedal range 
+    #fuzz.params[0].minimum / fuzz.params[0].maximum
+
+    #fuzz.params[0].value
+
+#
+# fuzz.toggle()
+# or
+# fuzz.active = not fuzz.active
+
+# mixer points
+# effects on wet path dry option later
+# delay wet to other delay
+# delay wet to [reverb .... 
+# delay post to
+# one effect is send?
+
+
+# delay 1 wet to delay 2,3,4. Reverb, output 1,2,3,4
+# collapsable mixer? only 1->1 shown?
+# delay 1 post to delay 2,3,4. Reverb, output 1,2,3,4
+
+
+# tape / tube, filter, eq, flange, bit crush
+
+
+# cv mixer
+
+
+# calculate tap tempo then
+# set transport state
+
+
